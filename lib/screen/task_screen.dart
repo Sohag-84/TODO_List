2db2305976950fcdda12e182bae981bac6f544c1
IH Sohag <injamulhaqsohag19977@gmail.com>
2022-03-18 18:53:06 +0600

    create list and refactor code

--- lib/screen/task_screen.dart
+++ lib/screen/task_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 class TaskScreen extends StatelessWidget {
   const TaskScreen({Key? key}) : super(key: key);
@@ -9,25 +8,74 @@ class TaskScreen extends StatelessWidget {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
 
+    bool check = false;
+
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: Container(
-        padding: const EdgeInsets.only(left: 30,top: 60,right: 30),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children:  [
-            const CircleAvatar(
-                child: Icon(Icons.list,size: 30,color: Colors.lightBlueAccent,),
-              backgroundColor: Colors.white,
-              radius: 30,
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        backgroundColor: Colors.lightBlueAccent, onPressed: () {  },
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(left: 30,top: 60,right: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children:  [
+                const CircleAvatar(
+                    child: Icon(Icons.list,size: 30,color: Colors.lightBlueAccent,),
+                  backgroundColor: Colors.white,
+                  radius: 30,
+                ),
+                SizedBox(height: height*0.01,),
+                const Text('Todoey',style:  TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 40),),
+                const Text('12 Tasks',style: TextStyle(color: Colors.white,fontSize: 17),),
+                SizedBox(height: height*0.02,)
+              ],
             ),
-            SizedBox(height: height*0.03,),
-            const Text('Todoey',style:  TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 40),),
-            const Text('12 Tasks',style: TextStyle(color: Colors.white,fontSize: 17),),
-           
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(30),topRight: Radius.circular(30))
+              ),
+              child:TaskList(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
-          ],
-        ),
+class TaskList extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        TaskTile(),
+        TaskTile(),
+      ],
+    );
+  }
+}
+
+class TaskTile extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: const Text('This is title'),
+      trailing: Checkbox(
+        value: false,
+        onChanged: (bool? value) {
+
+        },
       ),
     );
   }
